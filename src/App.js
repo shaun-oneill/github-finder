bd78c1a210246cf7e678d3644b2c1a6c2f54e6a1
Shaun O'Neill <oshauneill@hotmail.com>
2021-05-25 14:01:49 -0400

    alert for blank search query

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import axios from 'axios';
@@ -8,7 +9,8 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   // async componentDidMount() {
@@ -32,8 +34,15 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   }
 
-  //Clear users from state
+  // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false});
+
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  }
 
   render() {
     const { users, loading } = this.state;
@@ -42,10 +51,12 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search 
             searchUsers={this.searchUsers} 
             clearUsers={this.clearUsers} 
             showClear={users.length > 0 ? true : false} 
+            setAlert={this.setAlert}
           />
           <Users 
             loading={loading} 
