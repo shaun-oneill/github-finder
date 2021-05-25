71a02dc0e314b2903ad1f87a532e9525f5d9f5f1
Shaun O'Neill <oshauneill@hotmail.com>
2021-05-25 13:27:59 -0400

    clear users from state

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ class App extends Component {
 
   // Search Github users
   searchUsers = async (text) => {
+    this.setState({ loading: true });
+
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
@@ -30,13 +32,25 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   }
 
+  //Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false});
+
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search 
+            searchUsers={this.searchUsers} 
+            clearUsers={this.clearUsers} 
+            showClear={users.length > 0 ? true : false} 
+          />
+          <Users 
+            loading={loading} 
+            users={users} 
+          />
         </div>
       </div>
     );
